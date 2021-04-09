c33dbb6e9fd709e5747d8e3ac67c21980e9fc074
Ivan Borisov <miptivan@gmail.com>
2021-04-09 16:55:28 +0500

    add config and draw rectangle

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 import pygame
 import random
+import config
 
-WIDTH = 500  # ширина игрового окна
-HEIGHT = 500  # высота игрового окна
-FPS = 30  # частота кадров в секунду
-# Цвета (R, G, B)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (252, 250, 184)
-
-'''pygame.init()  # запускает pygame
-pygame.mixer.init()  # для звука
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()'''
-
+WIDTH, HEIGHT, FPS, RED, YELLOW = config.WIDTH, config.HEIGHT, config.FPS, config.RED, config.YELLOW
+clock = pygame.time.Clock()
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
@@ -25,11 +11,13 @@ display = pygame.display.set_mode((WIDTH, HEIGHT))
 def game_loop():
     running = True
     while running:
-        for event in pygame.event.get():
+        clock.tick(FPS)  # держим цикл на правильной скорости
+        for event in pygame.event.get():  # обработка событий
             if event.type == pygame.QUIT:
                 pygame.quit()  # завершить pygame
                 quit()  # выйти
         display.fill(YELLOW)
+        pygame.draw.rect(display, RED, (20, 20, 100, 100))
         pygame.display.update()
 
 
