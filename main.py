756564fd256724312c4b4087c7d3e80d5ca5139a
Ivan Borisov <miptivan@gmail.com>
2021-04-09 17:06:48 +0500

    add config and drab rectangle

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pygame
 import random
 import config
 
-WIDTH, HEIGHT, FPS, RED, YELLOW = config.WIDTH, config.HEIGHT, config.FPS, config.RED, config.YELLOW
+WIDTH, HEIGHT, FPS = config.WIDTH, config.HEIGHT, config.FPS
+RED, BLACK, LIGHT_YELLOW = config.RED, config.BLACK, config.LIGHT_YELLOW
 clock = pygame.time.Clock()
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -16,8 +17,10 @@ def game_loop():
             if event.type == pygame.QUIT:
                 pygame.quit()  # завершить pygame
                 quit()  # выйти
-        display.fill(YELLOW)
+        display.fill(LIGHT_YELLOW)
         pygame.draw.rect(display, RED, (20, 20, 100, 100))
+        pygame.draw.rect(display, BLACK, (50, 50, 150, 150))
+        pygame.draw.circle(display, RED, (100, 100), 50)
         pygame.display.update()
 
 
