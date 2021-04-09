53dba9e1602ca58f6185e4e9426bc9739b84e27a
Ivan Borisov <miptivan@gmail.com>
2021-04-09 17:19:15 +0500

    move rectangle

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import config
 
 WIDTH, HEIGHT, FPS = config.WIDTH, config.HEIGHT, config.FPS
-RED, BLACK, LIGHT_YELLOW = config.RED, config.BLACK, config.LIGHT_YELLOW
+RED, BLACK, GREEN, LIGHT_YELLOW = config.RED, config.BLACK, config.GREEN, config.LIGHT_YELLOW
 clock = pygame.time.Clock()
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -11,6 +11,7 @@ display = pygame.display.set_mode((WIDTH, HEIGHT))
 # Цикл игры
 def game_loop():
     running = True
+    x = 0
     while running:
         clock.tick(FPS)  # держим цикл на правильной скорости
         for event in pygame.event.get():  # обработка событий
@@ -18,10 +19,16 @@ def game_loop():
                 pygame.quit()  # завершить pygame
                 quit()  # выйти
         display.fill(LIGHT_YELLOW)
-        pygame.draw.rect(display, RED, (20, 20, 100, 100))
+
+        pygame.draw.rect(display, RED, (x, 20, 100, 100))
         pygame.draw.rect(display, BLACK, (50, 50, 150, 150))
-        pygame.draw.circle(display, RED, (100, 100), 50)
+        pygame.draw.circle(display, RED, (100, 200), 70)
+        pygame.draw.ellipse(display, GREEN, (300, 50, 280, 100))
         pygame.display.update()
+        if x <= WIDTH:
+            x += 10
+        else:
+            x = -100
 
 
 game_loop()
