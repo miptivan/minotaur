ef065dcee9af9bc9c19495167d6fcf67df3cc4bb
Ivan Borisov <miptivan@gmail.com>
2021-04-11 00:42:21 +0500

    add player controller and mask pressed keys

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import pygame
 import random
 import config
+import player
 
+# config
 WIDTH, HEIGHT, FPS = config.WIDTH, config.HEIGHT, config.FPS
 RED, BLACK, GREEN, LIGHT_YELLOW = config.RED, config.BLACK, config.GREEN, config.LIGHT_YELLOW
 clock = pygame.time.Clock()
 display = pygame.display.set_mode((WIDTH, HEIGHT))
+# / config
 
-# движение
-RIGHT = "r"  # r - right
-LEFT = "l"  # l - left
-UP = "u"  # u - up
-DOWN = "d"  # d - down
-STOP = "s"  # s - stop
-motion = STOP
-# /движение
+# player
+RIGHT, LEFT, UP, DOWN, STOP = player.RIGHT, player.LEFT, player.UP, player.DOWN, player.STOP
+motion = STOP  # начальное состояние
+speed = player.speed  # 10
+# / player
 
 
 # Цикл игры
@@ -25,12 +25,11 @@ def game_loop():
     userY = 300
     while running:
         clock.tick(FPS)  # держим цикл на правильной скорости
-
         for event in pygame.event.get():  # обработка событий
             if event.type == pygame.QUIT:
                 pygame.quit()  # завершить pygame
                 quit()  # выйти
-            elif event.type == pygame.KEYDOWN:
+            '''elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     motion = LEFT
                 elif event.key == pygame.K_RIGHT:
@@ -40,16 +39,41 @@ def game_loop():
                 elif event.key == pygame.K_DOWN:
                     motion = DOWN
             else:
-                motion = STOP
+                motion = STOP'''
 
-        if motion == LEFT:
-            userX -= 3
+        '''if motion == LEFT:
+            userX -= speed
         elif motion == RIGHT:
-            userX += 3
+            userX += speed
         elif motion == UP:
-            userY -= 3
+            userY -= speed
         elif motion == DOWN:
-            userY += 3
+            userY += speed
+        else:
+            pass'''
+
+        # перейти на векторы
+        keys = pygame.key.get_pressed()  # снимает "маску" зажатых клавиш. 1 - зажата, 0 - нет
+        if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
+            userX -= speed
+            userY -= speed
+        elif keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
+            userX -= speed
+            userY += speed
+        elif keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
+            userX += speed
+            userY -= speed
+        elif keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
+            userX += speed
+            userY += speed
+        elif keys[pygame.K_LEFT]:
+            userX -= speed
+        elif keys[pygame.K_RIGHT]:
+            userX += speed
+        elif keys[pygame.K_UP]:
+            userY -= speed
+        elif keys[pygame.K_DOWN]:
+            userY += speed
         else:
             pass
 
---
+++ player.py
@@ -0,0 +1,14 @@
+import pygame
+# движение
+RIGHT = "r"  # r - right
+LEFT = "l"  # l - left
+UP = "u"  # u - up
+DOWN = "d"  # d - down
+STOP = "s"  # s - stop
+motion = STOP
+speed = 10
+# /движение
+
+
+def to_right():
+    pass
