bcb6a9d71f15a9b16eacd9ee2330c0fb7aa48b5d
Rainory <gusev.ea@phystech.edu>
2021-04-13 16:49:43 +0300

    gui created

---
+++ game_loop.py
@@ -0,0 +1,83 @@
+import pygame
+import random
+import config
+import gui
+
+
+WIDTH, HEIGHT, FPS = config.WIDTH, config.HEIGHT, config.FPS
+RED, BLACK, GREEN, LIGHT_YELLOW = config.RED, config.BLACK, config.GREEN, config.LIGHT_YELLOW
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, player_imges, vel=3):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = player_imges
+        self.image = self.images['down_0']
+        self.state = 'down'
+        self.rect = self.image.get_rect()  # оценивает изображение и высчитывает прямоугольник, способный окружить его
+        self.rect.center = (config.WIDTH/2, config.HEIGHT/2)
+        self.vel = vel
+    
+    def change_img(img):
+        self.image = self.images['face']
+        #self.rect = self.image.get_rect()  # оценивает изображение и высчитывает прямоугольник, способный окружить его
+    
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.vel
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.vel
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.vel
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.vel
+
+
+# Цикл игры
+def game_loop():
+
+    clock = pygame.time.Clock()
+    display = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+    all_sprites = pygame.sprite.Group()
+
+    player_images = {
+        'down_0': pygame.image.load('assets/minotaur-S-stand.png').convert(),
+        'up_0': pygame.image.load('assets/minotaur-N-stand.png').convert(),
+        'right_0': pygame.image.load('assets/minotaur-W-stand.png').convert(),# надо создать еще Е картники
+        'right_1': pygame.image.load('assets/minotaur-W-step1.png').convert(),
+        'right_2': pygame.image.load('assets/minotaur-W-step2.png').convert(),
+        'left_0': pygame.image.load('assets/minotaur-W-stand.png').convert(),
+        'left_1': pygame.image.load('assets/minotaur-W-step1.png').convert(),
+        'left_2': pygame.image.load('assets/minotaur-W-step2.png').convert(),
+        'up_1': pygame.image.load('assets/minotaur-N-step1.png').convert(),
+        'up_2': pygame.image.load('assets/minotaur-N-step2.png').convert(),
+        'down_1': pygame.image.load('assets/minotaur-S-step1.png').convert(),
+        'down_2': pygame.image.load('assets/minotaur-S-step2.png').convert()
+    }
+
+    player = Player(player_images, 3)
+    all_sprites.add(player)
+
+
+    running = True
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()  # завершить pygame
+                quit()  # выйти
+
+        # мышь
+        pressed = pygame.mouse.get_pressed(num_buttons=3)  # (False, False, False)
+        pos = pygame.mouse.get_pos()
+        if pressed[0]:
+            print(pos)
+        # / мышь
+
+        player.move()
+        display.fill(LIGHT_YELLOW)
+        all_sprites.update()
+        all_sprites.draw(display)
+        pygame.display.update()
---
+++ gui.py
@@ -0,0 +1,134 @@
+import pygame
+import pygame_gui
+
+
+def find_game_menu():
+    pygame.init()
+    pygame.display.set_caption('Quick Start')
+    window_surface = pygame.display.set_mode((800, 600))
+
+    background = pygame.Surface((800, 600))
+    background.fill(pygame.Color('#000000'))
+
+    manager = pygame_gui.UIManager((800, 600))
+
+    text_element = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((300, 200), (200, 50)), 
+                                                    manager=manager)
+
+    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 270), (100, 50)),
+                                                text='Say Hello',
+                                                manager=manager)
+
+    clock = pygame.time.Clock()
+    is_running = True
+
+    while is_running:
+        time_delta = clock.tick(60)/1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+
+            manager.process_events(event)
+
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == hello_button:
+                        ip, port = text_element.text.split(':')
+                        is_running = False
+
+        manager.update(time_delta)
+
+        window_surface.blit(background, (0, 0))
+        manager.draw_ui(window_surface)
+
+        pygame.display.update()
+
+
+def main_menu():
+    pygame.init()
+    pygame.display.set_caption('Quick Start')
+    window_surface = pygame.display.set_mode((800, 600))
+
+    background = pygame.Surface((800, 600))
+    background.fill(pygame.Color('#000000'))
+
+    manager = pygame_gui.UIManager((800, 600))
+
+    new_game_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 190), (100, 50)),
+                                                text='New game',
+                                                manager=manager)
+    
+    find_game_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 310), (100, 50)),
+                                                    text='Find game',
+                                                    manager=manager)
+
+    clock = pygame.time.Clock()
+    is_running = True
+
+    while is_running:
+        time_delta = clock.tick(60)/1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+
+            manager.process_events(event)
+
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == find_game_button:
+                        find_game_menu()
+                        is_running = False
+                    if event.ui_element == new_game_button:
+                        res = new_game_menu()
+                        if res == 'go':
+                            return 'go'
+
+        manager.update(time_delta)
+
+        window_surface.blit(background, (0, 0))
+        manager.draw_ui(window_surface)
+
+        pygame.display.update()
+
+def new_game_menu():
+    pygame.init()
+    pygame.display.set_caption('Quick Start')
+    window_surface = pygame.display.set_mode((800, 600))
+
+    background = pygame.Surface((800, 600))
+    background.fill(pygame.Color('#000000'))
+
+    manager = pygame_gui.UIManager((800, 600))
+
+    schetchik = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((350, 190), (100, 50)),
+                                            text='kuku',
+                                            manager=manager)
+
+    start_game_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 310), (100, 50)),
+                                                text='Start game',
+                                                manager=manager)
+
+    clock = pygame.time.Clock()
+    is_running = True
+
+    while is_running:
+        time_delta = clock.tick(60)/1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+
+            manager.process_events(event)
+
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == start_game_button:
+                        print('The game is coming!!!')
+                        #is_running = False
+                        return 'go'
+
+        manager.update(time_delta)
+
+        window_surface.blit(background, (0, 0))
+        manager.draw_ui(window_surface)
+
+        pygame.display.update()
